16370acef05dd97058940951a3a6ef2a0c54d94f
abrochard <aaa.brochard@gmail.com>
2016-10-24 22:08:31 -0400

    that was a mistake...

--- Karma.js
+++ Karma.js
@@ -1,15 +1,3 @@
-
-// Not the cleanest but efficient way to load all the scripts
-document.write('<script type="text/javascript" src="Constants.js"></script>');
-document.write('<script type="text/javascript" src="CardMap.js"></script>');
-document.write('<script type="text/javascript" src="Card.js"></script>');
-document.write('<script type="text/javascript" src="Deck.js"></script>');
-document.write('<script type="text/javascript" src="Player.js"></script>');
-document.write('<script type="text/javascript" src="Game.js"></script>');
-document.write('<div style="display:none;"><img id="front" src="playingCards.png">' +
-               '<img id="back" src="playingCardBacks.png"></div>');
-
-
 function Karma(){
     var canvas = document.getElementById('board');
     if (canvas.getContext){
